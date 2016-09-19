89d0f03d33a0cf05cf559ca8949d09ce438bc467
peteris <peteris@citymapper.com>
2016-09-19 09:54:20 +0100

    Add iOS simulator alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# ios simulator
+alias simulator='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
